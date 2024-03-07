a4c97279a826cf113775c5d31c3aac1dcf903d39
Sebastian Küsters <sebastian@wemogy.com>
2024-03-07 15:59:01 +0100

    fix: changed open mode to r+

--- src/app.py
+++ src/app.py
@@ -36,7 +36,7 @@ def set_output(name: str, value: Any) -> None:
         return
 
     logger.info('Setting GitHub actions output %s=%s', name, value)
-    with open(os.environ['GITHUB_OUTPUT'], 'w') as fh:
+    with open(os.environ['GITHUB_OUTPUT'], 'r+') as fh:
         # remove line which starts with name=
         fh.seek(0)
         lines = fh.readlines()
